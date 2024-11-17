e063ca0951cf7e3a051d33301bb4bd6765cf35b8
tsamkharadze <samkaradze@gmail.com>
2024-11-17 09:02:21 +0400

    edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-// Resolve the directory path using import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(new URL("./src", import.meta.url).pathname),
+      "@": `${__dirname}/src`,
     },
   },
 });
